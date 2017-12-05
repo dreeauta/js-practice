eaff3f694e579a778e071979853077573108c0cb
Andreea Uta <dreea.9965@gmail.com>
2017-12-05 18:55:22 -0500

    updated sort

--- oddnumbers.js
+++ oddnumbers.js
@@ -12,26 +12,3 @@ function oddNumbers(l, r) {
 }
 
 oddNumbers(1,10);
-
-
-// sort
-
-var arr = [1,6,2,4,3,5,9]
-var newarr = [];
-
-function sorting(){
-  for (var i = 0; i <arr.length; i++){
-    for(var j = 0; j <arr.length -1; j++){
-      if (arr[j] > arr[i]){
-        temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-        }
-
-      }
-    }
-    return arr
-}
-
-sorting(arr);
-console.log(arr);
